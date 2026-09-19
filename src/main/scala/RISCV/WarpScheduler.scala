710d992a63cbd0cdb9b7ca22b662193584771c21
Liam Hanrahan <outercloudstudio@gmail.com>
2026-09-19 14:26:14 -0400

    add warps to fetch

--- src/main/scala/RISCV/WarpScheduler.scala
+++ src/main/scala/RISCV/WarpScheduler.scala
@@ -8,17 +8,10 @@ import scala.math._
 class WarpScheduler() extends Module {
     val io = IO(new Bundle {
         val active_warp = Output(UInt(2.W))
-        val active_pc = Output(UInt(32.W))
-
-        val next_pc = Input(UInt(32.W))
     })
 
 	val active_warp = RegInit(0.U(2.W))
+	val warp_enabled = RegInit(VecInit(Seq.fill(4.toInt)(true.B)))
 
 	io.active_warp := active_warp
-	
-	val warp_instruction_pointers = RegInit(VecInit(Seq.fill(4.toInt)(0.U(32.W))))
-
-	io.active_pc := warp_instruction_pointers(active_warp)
-	warp_instruction_pointers(active_warp) := io.next_pc
 }
--- src/main/scala/RISCV/pipeline/Fetch.scala
+++ src/main/scala/RISCV/pipeline/Fetch.scala
@@ -20,9 +20,7 @@ class Fetch() extends Module {
 	val io = IO(new Bundle {
 		val execute = Input(Bool())
 
-		val active_warp = Input(UInt(4.W))
-		val active_pc = Input(UInt(32.W))
-		val next_pc = Output(UInt(32.W))
+		val active_warp = Input(UInt(2.W))
 
 		val fetch_request = Input(new FetchReq)
 		val fetch_result = Output(Valid(new FetchResult))
@@ -34,14 +32,19 @@ class Fetch() extends Module {
 		val icache_data  = Input(UInt(32.W))
 	})
 
+	// RegInit(VecInit(Seq.fill(4.toInt)(0.U(32.W))))
+
+	val instruction_pointers = RegInit(VecInit(Seq.fill(4.toInt)(0.U(32.W))))
+	val ignore_instructions = RegInit(VecInit(Seq.fill(4.toInt)(false.B)))
+	
 	val request_instruction_pointer = RegInit(0.U(32.W))
-	val ignore_instruction = RegInit(false.B)
-	val in_flight = RegInit(false.B)
+	val request_warp = RegInit(0.U(2.W))
+	val request_in_flight = RegInit(false.B)
 
 	val fetch_result = Reg(new FetchResult)
 	val fetch_result_valid = RegInit(false.B)
 
-	io.icache_req.address := io.active_pc
+	io.icache_req.address := instruction_pointers(io.active_warp)
 	io.icache_req.op := MemOp.LW
 	io.icache_req.write_data := 0.U
 	io.icache_req.read := true.B
@@ -51,45 +54,42 @@ class Fetch() extends Module {
 	io.fetch_result.valid := fetch_result_valid
 	io.fetch_result.bits := fetch_result
 
-	io.next_pc := io.active_pc
-
 	val stalling = io.execute && io.fetch_request.fetch_op === FetchOp.ST
 	val dequeuing = io.execute && io.fetch_request.fetch_op === FetchOp.DQ
 	val redirecting = io.execute && io.fetch_request.fetch_op === FetchOp.RD
 
 	val can_issue = io.icache_ready && !fetch_result_valid
 
-	def issue(instruction_pointer: UInt): Unit = {
-		io.icache_req.address := instruction_pointer
-		io.icache_start := true.B
-		request_instruction_pointer := instruction_pointer
-		io.next_pc := instruction_pointer + 4.U
-	}
-
 	when(io.execute) {
 		when((dequeuing || stalling) && can_issue) {
-			issue(io.active_pc)
+			io.icache_req.address := instruction_pointers(io.active_warp)
+			io.icache_start := true.B
+			request_instruction_pointer := instruction_pointers(io.active_warp)
+			request_warp := io.active_warp
+			instruction_pointers(io.active_warp) := instruction_pointers(io.active_warp) + 4.U
 		}
 
 		when(redirecting) {
 			when(can_issue) {
-				issue(io.fetch_request.redirect_addr)
+				io.icache_req.address := io.fetch_request.redirect_addr
+				io.icache_start := true.B
+				request_instruction_pointer := io.fetch_request.redirect_addr
+				request_warp := io.active_warp
+				instruction_pointers(io.active_warp) := io.fetch_request.redirect_addr + 4.U
 			}.otherwise {
-				io.next_pc := io.fetch_request.redirect_addr
+				instruction_pointers(io.active_warp) := io.fetch_request.redirect_addr
 			}
-			
-			ignore_instruction := in_flight && !io.icache_valid
-		}
-	}
 
-	when(io.icache_valid && ignore_instruction) {
-		ignore_instruction := false.B
+			when(io.active_warp === request_warp) {
+				ignore_instructions(io.active_warp) := request_in_flight && !io.icache_valid
+			}
+		}
 	}
 
 	when(redirecting) {
 		fetch_result_valid := false.B        
 	}.otherwise {
-		when(io.icache_valid && !ignore_instruction) {
+		when(io.icache_valid && !ignore_instructions(request_warp)) {
 			fetch_result.pc   := request_instruction_pointer
 			fetch_result.inst := io.icache_data
 			fetch_result_valid := true.B
@@ -98,5 +98,9 @@ class Fetch() extends Module {
 		}
 	}
 
-	in_flight := io.icache_start || (!io.icache_valid && in_flight)
+	when(io.icache_valid && ignore_instructions(request_warp)) {
+		ignore_instructions(request_warp) := false.B
+	}
+
+	request_in_flight := io.icache_start || (!io.icache_valid && request_in_flight)
 }
